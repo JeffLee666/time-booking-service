4aadcec19f6026cc3624ae4f90298ede98f1caa4
JeffLee666 <52487348+JeffLee666@users.noreply.github.com>
2022-04-18 21:57:28 +0800

    Update TimeBookingServiceImpl.java

--- src/main/java/com/fantasy/tbs/service/impl/TimeBookingServiceImpl.java
+++ src/main/java/com/fantasy/tbs/service/impl/TimeBookingServiceImpl.java
@@ -80,4 +80,48 @@ public class TimeBookingServiceImpl implements TimeBookingService {
     public void bookTime(TimeBookDTO timeBookDTO) {
         timeBookingRepository.save(timeBookMapper.toTimeBooking(timeBookDTO));
     }
+    
+    @Override
+    @Transactional(readOnly = true)
+    public String getWorkTime(String personalNumber, ZonedDateTime startTime,
+                              ZonedDateTime endTime,  List<String> errorList) {
+
+        //TODO: Assumption: an employ should book time at least twice in a day. The earliest time
+        // is considered as start time while the latest time is end time. Also, if an employee books time
+        // less than twice, it would consider ZERO working hour on that day.
+        if (!checkIfParamsValid(personalNumber, startTime, endTime, errorList)) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String startTimeStr = getMidNightTime(startTime).format(formatter);
+        String endTimeStr = getMidNightTime(endTime.plusDays(1)).format(formatter);
+        //TODO: For better efficiency, a composite index included booking and personalNumber is suggested.
+        List<Double> workTimeList = timeBookingRepository.getWorkTime(personalNumber, startTimeStr, endTimeStr);
+        if (CollectionUtils.isEmpty(workTimeList)) {
+            String errorMessage = " there is no record for " + personalNumber;
+            errorList.add(errorMessage);
+            return null;
+        }
+        double totalWorkTimeInSec =  workTimeList.stream().mapToDouble(Double::doubleValue).sum();
+        double hours = totalWorkTimeInSec / 3600d;
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        return decimalFormat.format(hours);
+    }
+
+    private boolean checkIfParamsValid(String personalNumber, ZonedDateTime startTime,
+                                       ZonedDateTime endTime, List<String> errorList) {
+        if (StringUtil.isEmpty(personalNumber)
+            || Objects.isNull(startTime)
+            || Objects.isNull(endTime)) {
+            String errorMessage = "some parameters are invalid.";
+            errorList.add(errorMessage);
+            return false;
+        }
+        return true;
+    }
+
+    private ZonedDateTime getMidNightTime(ZonedDateTime dateTime) {
+        return ZonedDateTime.of(dateTime.getYear(), dateTime.getMonthValue(), dateTime.getDayOfMonth(),
+            0, 0, 0, 0, dateTime.getZone());
+    }
 }
